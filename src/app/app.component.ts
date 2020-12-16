5d4c53f2a7d47320342a10bc0e552c9a2f9f4f4a
Monteiro <arthur.monteiro@capgemini.com>
2020-12-16 18:40:35 -0300

    ajustes finos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,33 +22,39 @@ export class AppComponent {
   };
 
   error: boolean = false;
+  hasCat: boolean = false;
+
   errorMessage: string = "";
 
   constructor(private service: CatService) { }
 
   onSubmit(form: NgForm) {
-   
+
     if (form.valid) {
       this.service.getCat(form.value.cat)
         .subscribe((response: any) => {
           this.error = false;
+          this.hasCat = true;
           this.cat = response[0];
-          console.log();
+
+          if (this.cat == undefined) {
+            this.error = true;
+            this.hasCat = false;
+            this.errorMessage = "What the hell is that?! Well, definetly is not a cat!";
+          }
         }, (error: HttpErrorResponse) => {
           if (error.status == 404) {
             this.error = true;
+            this.hasCat = false;
+
             this.errorMessage = "Error 404";
             console.log(error);
           }
         });
-    } else {
-      if (!this.cat.name.valueOf()) {
-        this.error = true;
-        this.errorMessage = "What the hell is that?! Well, definetly is not a cat!";
-      }
     }
 
     this.error = true;
+    this.hasCat = false;
     this.errorMessage = "If you don't insert a cat breed, how can I guess??"
 
   }
